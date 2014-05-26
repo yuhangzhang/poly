dff5cb2ab152f0a75768b6ed059bb47ad0eac2af
yuhangzhang <paladined@gmail.com>
2014-05-26 11:05:40 +0200

    update the key to vector

--- poly.cpp
+++ poly.cpp
@@ -1,7 +1,7 @@
 #include "poly.h"
 
-#ifndef _BPOLY_CPP_
-#define _BPOLY_CPP_
+#ifndef _POLY_CPP_
+#define _POLY_CPP_
 
 template<class T>
 poly<T>::poly(int numvar, int order)
@@ -16,14 +16,23 @@ poly<T>::poly(int numvar, int order)
 template<class T>
 int poly<T>::termid(const vector<int>& vars)
 {
+	int id=0;
+	for(int i=1;i<=vars.size();i++)
+	{
+		id += combination(_numvar,i);
+	}
+
+	int order=vars.size();
 	for (vector<int>::const_iterator it = vars.begin(); it != vars.end(); it++) 
 	{
+		id += it
 	}
 }
 
 template<class T>
 int poly<T>::numterm()
 {
+	//clean 0 terms first
 	for(TERMS::iterator it=_polynomial.begin();it!=_polynomial.end();)
 	{
 		TERMS::iterator tit=it++;
@@ -32,6 +41,48 @@ int poly<T>::numterm()
 	}
 
 	return _polynomial.size();
+}
+
+template<class T>
+int poly<T>::maxsize()
+{
+	int total = 0;
+
+	for(int i=1;i<=_order;i++)
+	{
+		total += combination(_numvar,i);
+	}
+
+	return total;
+}
+
+template<class T>
+int poly<T>::combination(int m, int n)
+{
+	if(n>m)
+	{
+		return -1;
+	}
+	else if(n==0) 
+	{
+			return 1;
+	}
+	else
+	{
+		int total = 1;
+	
+		for(int i=m;i>m-n;i--)
+		{
+			total *= i;
+		}
+
+		for(int i=n;i>0;i--)
+		{
+			total /= i;
+		}
+
+		return total;
+	}
 }
 
 #endif
--- poly.h
+++ poly.h
@@ -13,11 +13,12 @@ template<class T>
 class poly
 {
 public:
-	typedef typename std::map<int,T> TERMS;
+	typedef typename std::map<vector<int>,T> TERMS;
 	poly(int numvar, int order);
 	int numterm();//return the number of terms with nonezero parameters
+	int maxsize();//return the max number of terms in this polynomial
 private:
-	int combination(int numvar, int order);//return the maximum number of terms in this polynomial
+	int combination(int m, int n);//return m choose n, m>=n
 	int termid(const vector<int>& vars);//return the serial number of the input term in this polynomial
 	TERMS _polynomial;//the real container of the polynoimal
 
